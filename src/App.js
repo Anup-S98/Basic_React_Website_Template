39b29c55ae5c3c9ac3b2b84be7253424ef31b737
Anup Saha <anupshaha4@gmail.com>
2022-11-28 21:14:22 +0530

    default route has been set

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
         <Route path='/service' element={<Service/>} />
         <Route path='/contact' element={<Contact/>} />
         <Route path="/" element={<Navigate replace to="/Home" />} />
+        <Route path="*" element={<Navigate to="/Home" replace />} />
       </Routes>
     </>
   );
